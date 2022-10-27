82c85cff92d439f5fdf0a53752745eb109d55490
Harun Guven <harun-626@hotmail.com>
2022-10-27 23:36:19 +0300

    add synch feature & new dummy data

--- src/App.js
+++ src/App.js
@@ -1,142 +1,161 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 
-import "./App.css";
-import questionsData from "./questionsData";
-import real_dummy_data from "./mock_data/real_dummy_data";
-import List from "./components/List/List";
-import { addQuestionToLocal, getQuestionData, getSlug } from "./helpers";
+import './App.css'
+import questionsData from './questionsData'
+import real_dummy_data from './mock_data/real_dummy_data'
+import List from './components/List/List'
+import {
+  addQuestionToLocal,
+  getQuestionData,
+  getSlug,
+  synchLocal,
+} from './helpers'
 
 function App() {
   // prettier-ignore
   const [questions, setQuestions] = useState({solved:[], examined:[], examineLater:[], inProgress:[]})
-  const [inputValue, setInputValue] = useState("");
-  const [isLocalMutated, setIsLocalMutated] = useState(false);
+  const [inputValue, setInputValue] = useState('')
+  const [isLocalMutated, setIsLocalMutated] = useState(false)
 
   useEffect(() => {
-    const solved = [];
-    const examined = [];
-    const examineLater = [];
-    const inProgress = [];
+    const solved = []
+    const examined = []
+    const examineLater = []
+    const inProgress = []
 
-    const localData = JSON.parse(localStorage.getItem("localQuestions"));
+    const localData = JSON.parse(localStorage.getItem('localQuestions'))
 
     if (localData) {
       for (const question of localData) {
-        if (question.status === "examined") {
-          examined.push(question);
-        } else if (question.status === "solved") {
-          solved.push(question);
-        } else if (question.status === "examineLater") {
-          examineLater.push(question);
-        } else if (question.status === "inProgress") {
-          inProgress.push(question);
+        if (question.status === 'examined') {
+          examined.push(question)
+        } else if (question.status === 'solved') {
+          solved.push(question)
+        } else if (question.status === 'examineLater') {
+          examineLater.push(question)
+        } else if (question.status === 'inProgress') {
+          inProgress.push(question)
         } else {
-          console.log("Fatal error! Look at App.js");
+          console.log('Fatal error! Look at App.js')
         }
       }
     } else {
-      localStorage.setItem("localQuestions", JSON.stringify([]));
+      localStorage.setItem('localQuestions', JSON.stringify([]))
     }
     // @prettier-ignore
-    setQuestions({ solved, examined, examineLater, inProgress });
-  }, [isLocalMutated]);
+    setQuestions({ solved, examined, examineLater, inProgress })
+  }, [isLocalMutated])
 
+  // ##  EVENT HANDLERS ##
   const handleChange = (event) => {
-    setInputValue(event.target.value);
-  };
+    setInputValue(event.target.value)
+  }
 
   const handleFocus = (event) => {
     navigator.clipboard
       .readText()
       .then((text) => {
-        const urlSlug = getSlug(text);
-        const questionData = getQuestionData(urlSlug, questionsData);
+        const urlSlug = getSlug(text)
+        const questionData = getQuestionData(urlSlug, questionsData)
         if (questionData) {
-          addQuestionToLocal(questionData);
-          setIsLocalMutated(!isLocalMutated);
-          setInputValue("");
+          addQuestionToLocal(questionData)
+          setIsLocalMutated(!isLocalMutated)
+          setInputValue('')
         }
       })
       .catch((err) => {
-        console.error("Failed to read clipboard contents: ", err);
-      });
-  };
+        console.error('Failed to read clipboard contents: ', err)
+      })
+  }
 
   const handleSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    const urlSlug = getSlug(inputValue);
-    const questionData = getQuestionData(urlSlug, questionsData);
+    const urlSlug = getSlug(inputValue)
+    const questionData = getQuestionData(urlSlug, questionsData)
     if (questionData) {
-      addQuestionToLocal(questionData);
-      setIsLocalMutated(!isLocalMutated);
-      setInputValue("");
+      addQuestionToLocal(questionData)
+      setIsLocalMutated(!isLocalMutated)
+      setInputValue('')
     }
-  };
+  }
 
   const handleChangeStatusTo = (status, displayId) => {
-    const localData = JSON.parse(localStorage.getItem("localQuestions"));
+    const localData = JSON.parse(localStorage.getItem('localQuestions'))
 
     const selectedQuestion = localData.find((question) => {
-      return question.displayId === displayId;
-    });
-    selectedQuestion.status = status;
-    localStorage.setItem("localQuestions", JSON.stringify(localData));
+      return question.displayId === displayId
+    })
+    selectedQuestion.status = status
+    localStorage.setItem('localQuestions', JSON.stringify(localData))
 
-    setIsLocalMutated(!isLocalMutated);
-  };
+    setIsLocalMutated(!isLocalMutated)
+  }
 
   const handleDummyData = (shouldUseDummy) => {
     if (shouldUseDummy) {
-      console.log("Fill dummy data");
-      localStorage.setItem("localQuestions", JSON.stringify(real_dummy_data));
+      console.log('Fill dummy data')
+      localStorage.setItem('localQuestions', JSON.stringify(real_dummy_data))
     } else {
-      console.log("Drop local storage");
-      localStorage.clear();
+      console.log('Drop local storage')
+      localStorage.clear()
     }
 
-    setIsLocalMutated(!isLocalMutated);
-  };
+    setIsLocalMutated(!isLocalMutated)
+  }
+
+  const handleUserData = () => {
+    navigator.clipboard
+      .readText()
+      .then((input) => {
+        synchLocal(input)
+        setIsLocalMutated(!isLocalMutated)
+      })
+      .catch((err) => {
+        console.error('Failed to read clipboard contents: ', err)
+      })
+  }
 
   return (
-    <div className="App">
-      <div className="Lists">
+    <div className='App'>
+      <div className='Lists'>
         <List
-          title="Questions Stack"
+          title='Questions Stack'
           questions={questions.inProgress}
           changeStatusHandler={handleChangeStatusTo}
         />
         <List
-          title="Solved"
+          title='Solved'
           questions={questions.solved}
           changeStatusHandler={handleChangeStatusTo}
         />
         <List
-          title="Examine Later"
+          title='Examine Later'
           questions={questions.examineLater}
           changeStatusHandler={handleChangeStatusTo}
         />
         <List
-          title="Examined"
+          title='Examined'
           questions={questions.examined}
           changeStatusHandler={handleChangeStatusTo}
         />
       </div>
       <form onSubmit={(event) => handleSubmit(event)}>
         <input
-          type="text"
+          type='text'
           onChange={handleChange}
           onFocus={handleFocus}
           value={inputValue}
           tabIndex={999}
           placeholder="Sadece tab'a basarak ekleyebilirsin.."
         />
-        <input type="submit" value="Add" />
+        <input type='submit' value='Add' />
       </form>
       <button onClick={() => handleDummyData(1)}>Dummy Data</button>
       <button onClick={() => handleDummyData()}>CE</button>
+      <button onClick={handleUserData}>Add Your Data</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/helpers.js
+++ src/helpers.js
@@ -1,41 +1,97 @@
 export function getSlug(url) {
-  let slug = "";
+  let slug = ''
   for (let i = 0; i < url.length; i++) {
-    if (url[i] === ".") {
-      slug = url.slice(i + 14, url.length - 1);
-      return slug;
+    if (url[i] === '.') {
+      slug = url.slice(i + 14, url.length - 1)
+      return slug
     }
   }
-  return false;
+  return false
 }
 
 export function getQuestionData(urlSlug, questionsData) {
-  let questionData = {};
+  let questionData = {}
 
   questionData = questionsData.find((question) => {
-    return question.urlSlug === urlSlug;
-  });
+    return question.urlSlug === urlSlug
+  })
 
-  const localData = JSON.parse(localStorage.getItem("localQuestions"));
+  const localData = JSON.parse(localStorage.getItem('localQuestions'))
   const isLocalQuestion = localData.some((question) => {
-    return question.urlSlug === urlSlug;
-  });
+    return question.urlSlug === urlSlug
+  })
 
   if (questionData && !isLocalQuestion) {
-    return questionData;
+    return questionData
   } else {
     console.log(
-      "You have already added this question before.\nOr this is bad input.\nExample input: https://leetcode.com/problems/two-sum/"
-    );
+      `You have already added this question before.
+      Or this is bad input.
+      Example input: https://leetcode.com/problems/two-sum/`
+    )
   }
 }
 
 export function addQuestionToLocal(questionData) {
   const newQuestion = {
     ...questionData,
-    status: "inProgress",
-  };
-  let localData = JSON.parse(localStorage.getItem("localQuestions"));
-  localData.push(newQuestion);
-  localStorage.setItem("localQuestions", JSON.stringify(localData));
+    status: 'inProgress',
+  }
+  let localData = JSON.parse(localStorage.getItem('localQuestions'))
+  localData.push(newQuestion)
+  localStorage.setItem('localQuestions', JSON.stringify(localData))
+}
+
+export function synchLocal(input) {
+  // turning string to json
+  const userData = JSON.parse(input)
+  // validating input
+  if (userData.stat_status_pairs === undefined) {
+    console.log(
+      'invalid data!!!\ngo to https://leetcode.com/api/problems/all/\nand copy data'
+    )
+    return
+  }
+
+  const localQuestions = JSON.parse(localStorage.getItem('localQuestions'))
+  const localQuestionsMap = {}
+  const userQuestions = userData.stat_status_pairs
+  const statusMap = {
+    ac: 'solved',
+    notac: 'examineLater',
+  }
+
+  // prepare hashmap of local questions to faster lookup
+  for (let i = 0; i < localQuestions.length; i++) {
+    localQuestionsMap[localQuestions[i].displayId] = i
+  }
+
+  // if user has no relation with question, skip next iteration stepx
+  for (let i = 0; i < userQuestions.length; i++) {
+    if (!userQuestions[i].status) {
+      continue
+    }
+
+    const question = userQuestions[i]
+    const questionStats = question.stat
+    const questionId = questionStats.frontend_question_id
+    const isFoundInLocal = localQuestionsMap.hasOwnProperty(questionId)
+
+    // if there is same question in local, only change its status
+    if (isFoundInLocal) {
+      const localQuestionIndex = localQuestionsMap[questionId]
+      console.log({ localQuestionIndex, question })
+      localQuestions[localQuestionIndex].status = statusMap[question.status]
+    } else {
+      const newQuestion = {
+        urlSlug: questionStats.question__title_slug,
+        displayId: questionId,
+        difficulty: question.difficulty.level,
+        status: statusMap[question.status],
+      }
+      localQuestions.push(newQuestion)
+    }
+  }
+
+  localStorage.setItem('localQuestions', JSON.stringify(localQuestions))
 }
